3ffe6e10f0025479d9c33f74c957813a2eb62653
Ming Lyu <leo@Mings-MacBook-Pro.local>
2024-01-02 16:55:54 -0800

    linear regression

---
+++ Linear Regression/linear_regression_numpy.py
@@ -0,0 +1,26 @@
+import numpy as np
+
+def linear_regression_noreg(X, y):
+    """
+  Compute the weight parameter given X and y.
+  Inputs:
+  - X: A numpy array of shape (num_samples, D) containing features
+  - y: A numpy array of shape (num_samples, ) containing labels
+  Returns:
+  - w: a numpy array of shape (D, )
+  """
+    w = np.matmul(np.linalg.inv(np.matmul(X.T, X)), np.matmul(X.T, y))
+    return w
+
+def regularized_linear_regression(X, y, lambd):
+    """
+    Compute the weight parameter given X, y and lambda.
+    Inputs:
+    - X: A numpy array of shape (num_samples, D) containing features
+    - y: A numpy array of shape (num_samples, ) containing labels
+    - lambd: a float number specifying the regularization parameter
+    Returns:
+    - w: a numpy array of shape (D, )
+    """
+    w = np.matmul(np.linalg.inv(np.matmul(X.T, X) + lambd * np.identity(X.shape[1])), np.matmul(X.T, y))
+    return w
