5dd08cc22b2325eb87357550b6bfa5a3e9cdcc68
Ming Lyu <leo@Mings-MacBook-Pro.local>
2024-01-03 21:32:12 -0800

    logistic regression

--- Logistic Regression/logistic_regression_numpy.py
+++ Logistic Regression/logistic_regression_numpy.py
@@ -1,45 +1,32 @@
 import numpy as np
+    
+def sigmoid_activation(x):
+    return 1/(1 + np.exp(-x))
 
-class LogisticRegression:
-    def __init__(self, lr=0.01, num_iter=100000, fit_intercept=True, verbose=False):
-        self.lr = lr
-        self.num_iter = num_iter
-        self.fit_intercept = fit_intercept
-        self.verbose = verbose
+def predict_prob(theta, X):
+    return sigmoid_activation(np.dot(X, theta))
 
-    def __add_intercept(self, X):
-        intercept = np.ones((X.shape[0], 1))
-        return np.concatenate((intercept, X), axis=1)
+def predict(theta, X, threshold=0.5):
+    return predict_prob(theta, X) >= threshold
 
-    def __sigmoid(self, z):
-        return 1 / (1 + np.exp(-z))
+def model_fit(X, y, lr, num_iter):
+    # initialize weight
+    
+    intercept = np.ones((X.shape[0], 1))
+    X = np.concatenate((intercept, X), axis=1)
+    theta = np.zeros(X.shape[1])
+    
+    prev = theta
 
-    def __loss(self, h, y):
-        return (-y * np.log(h) - (1 - y) * np.log(1 - h)).mean()
+    for i in range(num_iter):
+        z = np.dot(X, theta)
+        h = sigmoid_activation(z)
+        gradient = np.dot(X.T,(y - h))/ y.shape[0]
+        theta += lr * gradient
 
-    def fit(self, X, y):
-        if self.fit_intercept:
-            X = self.__add_intercept(X)
+        if i % 1000 == 0:
+            y_pred = predict(theta, X)
+            print(i, np.sum(y_pred==y)/y.shape[0])
 
-        # weights initialization
-        self.theta = np.zeros(X.shape[1])
-
-        for i in range(self.num_iter):
-            z = np.dot(X, self.theta)
-            h = self.__sigmoid(z)
-            gradient = np.dot(X.T, (h - y)) / y.size
-            self.theta -= self.lr * gradient
-
-            if(self.verbose == True and i % 10000 == 0):
-                z = np.dot(X, self.theta)
-                h = self.__sigmoid(z)
-                print(f'loss: {self.__loss(h, y)} \t')
-
-    def predict_prob(self, X):
-        if self.fit_intercept:
-            X = self.__add_intercept(X)
-
-        return self.__sigmoid(np.dot(X, self.theta))
-
-    def predict(self, X, threshold=0.5):
-        return self.predict_prob(X) >= threshold
+    return theta
+    
